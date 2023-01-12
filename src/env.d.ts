a1264a7eba7766f0195c7c838608f1205f5f1096
장용호 <jyh990605@gmail.com>
2023-01-12 17:19:58 +0900

    feat(env): default setting env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,10 @@
-interface ImportMeta {
+interface ImportMetaEnv {
   readonly VITE_BASE_URL: string;
 
   // API Server
   readonly VITE_API_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
