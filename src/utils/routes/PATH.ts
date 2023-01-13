be2587d4e7191b5394d7fdb5c8a6fe62c376b99f
장용호 <jyh990605@gmail.com>
2023-01-13 23:28:04 +0900

    PATH : add to a new path

--- src/utils/routes/PATH.ts
+++ src/utils/routes/PATH.ts
@@ -1,5 +1,12 @@
 const PATH = {
+  // send to API Server
+  URL: import.meta.env.VITE_API_BASE_URL,
+
+  // UnAuth API routes
   AUTH: "auth",
+
+  // protected API routes
+  TODO: "todos",
 };
 
 export default PATH;
